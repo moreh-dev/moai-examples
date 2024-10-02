b71568a146993388db5f5fe37b276b8511d0be89
whitewave99 <jaeyoung.jung@moreh.io>
2024-10-02 17:49:35 +0900

    edit script

--- run_internlm.sh
+++ run_internlm.sh
@@ -4,7 +4,7 @@ python train_th.py \
   --epochs 1 \
   --batch-size 16 \
   --log-interval 5 \
-  --block-size 1024 \
+  --block-size 32768 \
   --model-name-or-path /nas/team_cx/checkpoints/internlm2_5-20b-chat-unfused \
   --dataset-name-or-path agileloop/izaz-sequence-of-actions-prediction-dataset-llama2-7b-32k \
   --save-path /nas/team_cx/checkpoints/internlm2_5-20b-chat-finetuned
