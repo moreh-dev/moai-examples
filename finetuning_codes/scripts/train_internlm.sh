ba4a5d8dea72d2e2b9660d2a12f97df1a856ea33
whitewave99 <jaeyoung.jung@moreh.io>
2024-10-14 14:09:01 +0900

    Fix the scripts to absolute path

--- finetuning_codes/scripts/train_internlm.sh
+++ finetuning_codes/scripts/train_internlm.sh
@@ -3,7 +3,7 @@
 START_TIME=$(TZ="Asia/Seoul" date)
 current_time=$(date +"%y%m%d_%H%M%S")
 
-python train_internlm.py \
+python /root/poc/finetuning_codes/train_internlm.py \
   --epochs 1 \
   --train-batch-size 16 \
   --log-interval 5 \
@@ -12,7 +12,7 @@ python train_internlm.py \
   --dataset-name-or-path agileloop/izaz-sequence-of-actions-prediction-dataset-llama2-7b-32k \
   --save-path /root/poc/checkpoints/internlm2_5-20b-chat-finetuned \
   --max-step -1 \
-  |& tee logs/internlm_finetune_$current_time.log
+  |& tee /root/poc/finetuning_codes/logs/internlm_finetune_$current_time.log
 
 echo "Start: $START_TIME"
 echo "End: $(TZ="Asia/Seoul" date)"
