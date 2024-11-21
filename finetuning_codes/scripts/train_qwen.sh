f4c6f7bd443c31fe03c80d12e6b4f153c83528b5
jingee.kim <jingee.kim@moreh.io>
2024-11-21 16:09:18 +0900

    change to absolute path

--- finetuning_codes/scripts/train_qwen.sh
+++ finetuning_codes/scripts/train_qwen.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-export HF_HOME=/nas/team_cx/jingee/datasets
-
 START_TIME=$(TZ="Asia/Seoul" date)
 current_time=$(date +"%y%m%d_%H%M%S")
 
 TRANSFORMERS_VERBOSITY=info accelerate launch \
-    --config_file /nas/team_cx/jingee/ambre-models/finetuning_codes/config.yaml \
-    /nas/team_cx/jingee/ambre-models/finetuning_codes/train.py \
-    --model Qwen/Qwen-14B \
+    --config_file /root/poc/finetuning_codes/config.yaml \
+    /root/poc/finetuning_codes/train.py \
+    --model /root/poc/pretrained_models/Qwen-14B \
     --dataset alespalla/chatbot_instruction_prompts \
     --lr 0.0001 \
     --train-batch-size 64 \
     --eval-batch-size 1 \
     --num-epochs 5 \
-    --max-steps 100 \
-    --log-interval 2 \
-    --save-path /nas/team_cx/jingee/ambre-models/checkpoints/qwen_finetuned_$current_time \
-    |& tee /nas/team_cx/jingee/ambre-models/finetuning_codes/logs/qwen_finetune_$current_time.log
+    --max-steps 20 \
+    --log-interval 20 \
+    --save-path /root/poc/checkpoints/qwen_finetuned_$current_time \
+    |& tee /root/poc/finetuning_codes/logs/qwen_finetune_$current_time.log
 
 echo "Start: $START_TIME"
 echo "End: $(TZ="Asia/Seoul" date)"
