267d5572962b066e4718458da0f7735ae6ef0d28
moi-gwangho <gwangho.choi@moreh.io>
2025-01-02 12:56:33 +0900

    update utils: add correction ratio for Qwen

--- finetuning_codes/utils.py
+++ finetuning_codes/utils.py
@@ -92,7 +92,7 @@ def load_model(args):
     """
 
     print(f"Loading {args.model_name_or_path} Tokenizer...")
-    set_mem_usage_correction_ratio(args)
+    # set_mem_usage_correction_ratio(args)
     configs = AutoConfig.from_pretrained(args.model_name_or_path,
                                          trust_remote_code=True)
     if "baichuan" in configs.architectures[0].lower():
@@ -118,6 +118,7 @@ def load_model(args):
         tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path,
                                                   trust_remote_code=True)
     elif "qwen" in configs.architectures[0].lower():
+        moreh_config.set_config("advanced_parallelization_memory_usage_correction_ratio", 70)
         model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path,
                                                      trust_remote_code=True,
                                                      torch_dtype='float32', fp32=True)
