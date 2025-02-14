b365fae47c5fde93eaa40985fc95f8406249ae6f
Hugh Namkoong <mmsori90@gmail.com>
2025-02-14 04:57:08 +0000

    Add Mistral 7B

--- finetuning_codes/scripts/train_mistral_7b.sh
+++ finetuning_codes/scripts/train_mistral_7b.sh
@@ -4,19 +4,19 @@ START_TIME=$(TZ="Asia/Seoul" date)
 current_time=$(date +"%y%m%d_%H%M%S")
 
 TOKENIZERS_PARALLELISM=false TRANSFORMERS_VERBOSITY=info accelerate launch \
-    --config_file /root/poc/finetuning_codes/config.yaml \
-    /root/poc/finetuning_codes/train.py \
-    --model /models/Mistral-7B-v0.3 \
+    --config_file $CONFIG_PATH \
+    train.py \
+    --model mistralai/Mistral-7B-v0.3 \
     --dataset bitext/Bitext-customer-support-llm-chatbot-training-dataset \
-    --lr 0.000001 \
+    --lr 0.00001 \
     --train-batch-size 64 \
     --eval-batch-size 32 \
     --block-size 1024 \
     --num-epochs 5 \
     --max-steps -1 \
     --log-interval 20 \
-    --save-path /root/poc/checkpoints/mistral \
-    |& tee /root/poc/logs/mistral_$current_time.log
+    --save-path $SAVE_DIR \
+    |& tee $LOG_DIR
 
 echo "Start: $START_TIME"
 echo "End: $(TZ="Asia/Seoul" date)"
